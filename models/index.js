15a2eb44995de0806c6f628ffa7130db015fabe4
Simone Anthony <simone.anthony1@yahoo.com>
2022-01-03 21:38:01 -0500

    package lock added to git ignore, belongsToMany added to models, sync force set to false

--- models/index.js
+++ models/index.js
@@ -5,14 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Category.hasMany(Product);
 Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
